eaf6d216e19fa4e3faf430bed5a5245482f18b8b
Toby <toby@tobias.codes>
2022-09-17 22:45:32 +0200

    try async file stream

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 var fs = require('fs'),
     http = require('http');
 
-function sendStaticFile(res, filepath) {
-    fs.readFile(__dirname + '/' + filepath, function (err, data) {
-        if (res.writeableEnded) {
-            if (!err) {
-                res.write(data);
-                console.log('hey');
-            } else {
-                res.writeHead(404);
-                res.end(JSON.stringify(err));
-                res.log(JSON.stringify(err));
+async function sendStaticFiles(res, filepaths) {
+    for (var filepath of filepaths) {
+        fs.readFile(__dirname + '/' + filepath, function (err, data) {
+            if (res.writeableEnded) {
+                if (!err) {
+                    res.write(data);
+                    console.log(filepath + ' found')
+                } else {
+                    res.writeHead(404);
+                    res.end(JSON.stringify(err));
+                    res.log(JSON.stringify(err));
+                    console.log(filepath + ' not found')
+                }
             }
-        }
-    });
+        });
+    }
 }
 
 
@@ -29,10 +32,11 @@ const requestListener = function (request, response) {
     }).on('end', () => {
         body = Buffer.concat(body).toString();
         if (method == "GET") {
-            response.writeHead(200);
-            sendStaticFile(response, 'index.html');
-            sendStaticFile(response, 'styles.css');
-            response.end();
+            (async () => {
+                response.writeHead(200);
+                await sendStaticFiles(response, ['index.html', 'styles.css']);
+                response.end();
+            })();
         }
         if (method == "POST") {
             response.writeHead(200);
