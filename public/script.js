c8e232b43d3f58381f57600df43750c991df1896
Toby <toby@tobias.codes>
2022-09-18 14:05:29 +0200

    added join input box

--- public/script.js
+++ public/script.js
@@ -3,29 +3,38 @@ startgame.addEventListener("click", function () {
   fetch("/game_init", {
     method: "POST",
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ 'startgame': true })
+    body: JSON.stringify({ 'type': 'init' })
   }).then(function (response) {
     return response.text();
   }).then(function (data) {
     console.log('startgame body:');
     console.log(JSON.parse(data.toString()));
-    if (data['startgame'] == true) {
+    if (data['type'] == 'init') {
       var socket = io();
       socket.emit('join', data['room']);
+      socket.on('room full', (msg) => {
+        console.log('message: ' + msg);
+      });
     }
   });
 });
 
 const joingame = document.getElementById("joingame");
 joingame.addEventListener("click", function () {
-  fetch("/game_init", {
-    method: "POST",
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ 'joingame': true })
-  }).then(function (response) {
-    return response.text();
-  }).then(function (data) {
-    console.log('joingame body:');
-    console.log(data);
+  var socket = io();
+  var roomnum = Number.parseInt(document.getElementById('room').value);
+  if (roomnum != undefined && roomnum >= 1000) socket.emit('join', roomnum);
+  socket.on('room full', (msg) => {
+    console.log('message: ' + msg);
   });
+  // fetch("/join", {
+  //   method: "GET",
+  //   headers: { 'Content-Type': 'application/json' },
+  //   body: JSON.stringify({ 'type': 'join' })
+  // }).then(function (response) {
+  //   return response.text();
+  // }).then(function (data) {
+  //   console.log('joingame body:');
+  //   console.log(data);
+  // });
 });
--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.use(express.static('public'))
+// app.use('/join', express.static('join'))
+
 
 app.get('/favicon', (req, res) => {
     res.sendFile('favicon.ico');
 })
 
-var gameid = 0;
+var gameid = 1000;
+var numplayers = {};
 
 app.post('/game_init', (req, res) => {
     const { headers, method, url } = req;
@@ -24,17 +27,35 @@ app.post('/game_init', (req, res) => {
     }).on('end', () => {
         body = Buffer.concat(body).toString();
         body = JSON.parse(body);
-        body['room'] = gameid;
-        gameid += 1;
+        if (body['type'] == 'init') {
+            body['room'] = gameid;
+            gameid += 1;
+        } else if (body['type'] == 'join') {
+            app.use(express.static('public'))
+        }
         res.writeHead(200);
         res.end(Buffer.from(JSON.stringify(body)));
     });
 });
 
 io.sockets.on('connection', function (socket) {
+    console.log('connected');
     socket.on('join', function (room) {
-        socket.join(room);
-        console.log('joined room ' + r0oom.toString())
+        if (numplayers[room] < 2) {
+            socket.join(room);
+            socket.room = room;
+            if (numplayers[room] == undefined) {
+                numplayers[room] = 1;
+            } else {
+                numplayers[room] += 1;
+            }
+            console.log('joined room ' + room.toString())
+            console.log('there are now ' + numplayers[room] + ' players')
+            if (numplayers[room] == 2) {
+                io.sockets.in(room).emit('room full', 'true');
+            }
+        }
     });
 });
+
 server.listen(3000);
