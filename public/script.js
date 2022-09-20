4ca0b3525a7cdf68bbc4a99548b3f07cdac43866
Toby <toby@tobias.codes>
2022-09-20 12:33:40 +0200

    try module import time

--- public/script.js
+++ public/script.js
@@ -1,23 +1,4 @@
-class TimeFormat {
-  constructor(min, sec, inc) {
-    this.min = min;
-    this.sec = sec;
-    this.mil = 0;
-    this.inc = inc;
-    this.current_mil = 1000 * (60 * min + sec);
-  }
-  print() {
-    console.log(this.min, this.sec, this.mil, this.inc, this.current_mil);
-  }
-  printPretty() {
-    const m = Math.floor(this.current_mil / 60000);
-    const s = Math.floor((this.current_mil / 1000) - m);
-    console.log(m + ":" + s);
-  }
-  updateTime(movetime) {
-    this.current_mil -= movetime;
-  }
-}
+import './time.mjs';
 
 var socket;
 var mymove = false;
