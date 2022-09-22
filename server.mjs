945cb8725a33b47a83ec2bbaded0bbdae227e62a
Toby <toby@tobias.codes>
2022-09-22 21:47:11 +0200

    fixed scss (added npm sass on server side to compile)

--- server.mjs
+++ server.mjs
@@ -17,9 +17,6 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-var sass = require('node-sass');
-sass.render({file: './public/styles.scss'});
-
 app.use(express.static('public'));
 
 app.set('views', './views')
