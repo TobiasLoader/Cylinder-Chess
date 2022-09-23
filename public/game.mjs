ffe294b032cd33c86a3bd45a54eff7ca9d6ec598
Toby <toby@tobias.codes>
2022-09-23 18:11:12 +0200

    refactor and begin rotate on scroll

---
+++ public/game.mjs
@@ -0,0 +1,5 @@
+const cylinderarea = document.getElementById("cylinderarea");
+
+cylinderarea.addEventListener("scroll", function () {
+  cylinderarea.style.animation = 'rot-y 12s linear infinite';
+});
